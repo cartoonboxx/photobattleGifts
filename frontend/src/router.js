32defed319541026cf4a88afb94e598e6d4a5f60
cartoonboxx <shulenin_2005@mail.ru>
2025-08-19 17:03:10 +0300

    add main router

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,8 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
 import PricePageComponent from "@/page/prize/PricePageComponent.vue";
+import App from "@/App.vue";
 
-const routes = [{ path: "/prize/:id", component: PricePageComponent }];
-
+const routes = [
+  { path: "/", component: App },
+  { path: "/prize/:id", component: PricePageComponent },
+];
 const router = createRouter({
   history: createWebHistory(),
   routes,
